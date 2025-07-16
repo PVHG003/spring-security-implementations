ba4f419389ec0ebf76ad41208f49623df8f3d3ab
PVHG <phamhung155tp@gmail.com>
2025-07-16 11:00:14 +0700

    Implemented Oauth Authorization Server with in memory users

---
+++ authorization-server/src/main/java/vn/pvhg/authorizationserver/JwkSetRestController.java
@@ -0,0 +1,22 @@
+package vn.pvhg.authorizationserver;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class JwkSetRestController {
+
+    private final JWKSet jwkSet;
+
+    public JwkSetRestController(JWKSet jwkSet) {
+        this.jwkSet = jwkSet;
+    }
+
+    @GetMapping("/.well-known/jwks.json")
+    public Map<String, Object> keys() {
+        return jwkSet.toJSONObject();
+    }
+}
---
+++ authorization-server/src/main/java/vn/pvhg/authorizationserver/RSAKeyProperties.java
@@ -0,0 +1,13 @@
+package vn.pvhg.authorizationserver;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@ConfigurationProperties(prefix = "rsa")
+public record RSAKeyProperties(
+        RSAPublicKey publicKey,
+        RSAPrivateKey privateKey
+) {
+}
